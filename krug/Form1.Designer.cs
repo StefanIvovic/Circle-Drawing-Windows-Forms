f8fa0411397b5ca18dfec0f16edbfbf830b1295c
Stefan Ivovic <stefanivovic91@gmail.com>
2017-04-01 22:57:01 +0200

    Add project files.

---
+++ krug/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace krug
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tb
+            // 
+            this.tb.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb.Location = new System.Drawing.Point(12, 12);
+            this.tb.Multiline = true;
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(551, 476);
+            this.tb.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 300;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 500);
+            this.Controls.Add(this.tb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ krug/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace krug
+{
+    public partial class Form1 : Form
+    {
+        private int visina = 25;
+        public int duzina = 25;
+        private int precnik = 10;
+        private char[,] matrica;
+        private int r = 0;
+        private bool unapred = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Enabled = true;
+        }
+
+        public void asdf()
+        {
+           
+            for (int u = 0; u <= 360; u++)
+            {
+                int x = (int)(r *Math.Sin(Math.PI * u / 180.0));
+                int y = (int) (r*Math.Cos(Math.PI*u/180.0));
+                matrica[x+visina/2, y+duzina/2] = 'X';
+            }
+            printMatra();
+                
+            matra();
+            if(unapred && r < precnik)
+                r++;
+            else if (r > 0)
+            {
+                r--;
+                unapred = false;
+            }
+               
+            else
+            {
+                r++;
+                unapred = true;
+            }
+
+        }
+
+        public void matra()
+        {
+            matrica = new char[visina,duzina];
+            for (int i = 0; i<visina; i++)
+                for (int j = 0; j < duzina; j++)
+                    matrica[i, j] = '.';
+        }
+
+        public void printMatra()
+        {
+            tb.Text = "";
+            for (int i = 0; i < visina; i++)
+            {
+                for (int j = 0; j < duzina; j++)
+                    tb.Text += matrica[i, j]+" ";
+                tb.Text += "\r\n";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            matra();
+            asdf();
+        }
+    }
+}
